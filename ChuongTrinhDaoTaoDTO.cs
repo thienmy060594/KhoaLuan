ff2d5e6c5b1feed41eea562ceed61a3ec96b263c
thienmy060594 <41846280+thienmy060594@users.noreply.github.com>
2020-05-25 21:51:08 +0700

    Add files via upload

--- ChuongTrinhDaoTaoDTO.cs
+++ ChuongTrinhDaoTaoDTO.cs
@@ -11,8 +11,11 @@ namespace KiemDinhChatLuongDTO
     {
         private int id_chuongtrinhdaotao;       
         private int id_nganh;
+        private int id_tailieu;
         private string manganh;
         private string tennganh;
+        private string matailieu;
+        private string tentailieu;
         private string machuongtrinhdaotao;
         private string namky;
         private string namapdung;
@@ -20,20 +23,26 @@ namespace KiemDinhChatLuongDTO
         private string ghichu;
         public int Id_ChuongTrinhDaoTao { get => id_chuongtrinhdaotao; set => id_chuongtrinhdaotao = value; }        
         public int Id_Nganh { get => id_nganh; set => id_nganh = value; }
+        public int Id_TaiLieu { get => id_tailieu; set => id_tailieu = value; }
         public string MaNganh { get => manganh; set => manganh = value; }
         public string TenNganh { get => tennganh; set => tennganh = value; }
+        public string MaTaiLieu { get => matailieu; set => matailieu = value; }
+        public string TenTaiLieu { get => tentailieu; set => tentailieu = value; }
         public string MaChuongTrinhDaoTao { get => machuongtrinhdaotao; set => machuongtrinhdaotao = value; }
         public string NamKy { get => namky; set => namky = value; }
         public string NamApDung { get => namapdung; set => namapdung = value; }
         public string TomTatNoiDung { get => tomtatnoidung; set => tomtatnoidung = value; }
         public string GhiChu { get => ghichu; set => ghichu = value; }
 
-        public ChuongTrinhDaoTaoDTO(int id_chuongtrinhdaotao, int id_nganh, string manganh, string tennganh, string machuongtrinhdaotao, string namky, string namapdung, string tomtatnoidung, string ghichu)
+        public ChuongTrinhDaoTaoDTO(int id_chuongtrinhdaotao, int id_nganh, int id_tailieu, string manganh, string tennganh, string matailieu, string tentailieu, string machuongtrinhdaotao, string namky, string namapdung, string tomtatnoidung, string ghichu)
         {
             this.Id_ChuongTrinhDaoTao = id_chuongtrinhdaotao;            
             this.Id_Nganh = id_nganh;
+            this.Id_TaiLieu = id_tailieu;
             this.MaNganh = manganh;
             this.TenNganh = tennganh;
+            this.MaTaiLieu = matailieu;
+            this.TenTaiLieu = tentailieu;
             this.MaChuongTrinhDaoTao = machuongtrinhdaotao;
             this.NamKy = namky;
             this.NamApDung = namapdung;
@@ -45,8 +54,11 @@ namespace KiemDinhChatLuongDTO
         {
             this.Id_ChuongTrinhDaoTao = Int32.Parse(row["ID_ChuongTrinhDaoTao"].ToString());            
             this.Id_Nganh = Int32.Parse(row["ID_Nganh"].ToString());
+            this.Id_TaiLieu = Int32.Parse(row["ID_TaiLieu"].ToString());
             this.MaNganh = row["MaNganh"].ToString();
             this.TenNganh = row["TenNganh"].ToString();
+            this.MaTaiLieu = row["MaTaiLieu"].ToString();
+            this.TenTaiLieu = row["TenTaiLieu"].ToString();
             this.MaChuongTrinhDaoTao = row["MaChuongTrinhDaoTao"].ToString();
             this.NamKy = row["NamKy"].ToString();
             this.NamApDung = row["NamApDung"].ToString();
---
+++ MonTienQuyetDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace KiemDinhChatLuongDTO
+{
+    public class MonTienQuyetDTO
+    {
+        private int id_monhoc;
+        private int id_monhoc_tienquyet;
+        private string mamonhoc;
+        private string tenmonhoc;
+        private string mamonhoctienquyet;
+        private string tenmonhoctienquyet;
+        private string ghichu;
+
+        public int Id_MonHoc { get => id_monhoc; set => id_monhoc = value; }
+        public int Id_MonHoc_TienQuyet { get => id_monhoc_tienquyet; set => id_monhoc_tienquyet = value; }
+        public string MaMonHoc { get => mamonhoc; set => mamonhoc = value; }
+        public string TenMonHoc { get => tenmonhoc; set => tenmonhoc = value; }
+        public string MaMonHocTienQuyet { get => mamonhoctienquyet; set => mamonhoctienquyet = value; }
+        public string TenMonHocTienQuyet { get => tenmonhoctienquyet; set => tenmonhoctienquyet = value; }
+        public string GhiChu { get => ghichu; set => ghichu = value; }
+
+        public MonTienQuyetDTO(int id_monhoc, int id_monhoc_tienquyet, string mamonhoc, string tenmonhoc, string mamonhoctienquyet, string tenmonhoctienquyet, string ghichu)
+        {
+            this.Id_MonHoc = id_monhoc;
+            this.Id_MonHoc_TienQuyet = id_monhoc_tienquyet;
+            this.MaMonHoc = mamonhoc;
+            this.TenMonHoc = tenmonhoc;
+            this.MaMonHocTienQuyet = mamonhoctienquyet;
+            this.TenMonHocTienQuyet = tenmonhoctienquyet;
+            this.GhiChu = ghichu;
+        }
+
+        public MonTienQuyetDTO(DataRow row)
+        {
+            this.Id_MonHoc = Int32.Parse(row["ID_MonHoc"].ToString());
+            this.Id_MonHoc_TienQuyet = Int32.Parse(row["ID_MonHoc_TienQuyet"].ToString());
+            this.MaMonHoc = row["MaMonHoc"].ToString();
+            this.TenMonHoc = row["TenMonHoc"].ToString();
+            this.MaMonHocTienQuyet = row["MaMonHocTienQuyet"].ToString();
+            this.TenMonHocTienQuyet = row["TenMonHocTienQuyet"].ToString();
+            this.GhiChu = row["GhiChu"].ToString();
+        }
+    }
+}
